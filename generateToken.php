4ce6508e08314e1b641a66792fc9a4491512474a
nemanjamil <nemanjamil@gmail.com>
2019-03-01 19:46:54 +0100

    Modifications according to ROB request

--- generateToken.php
+++ generateToken.php
@@ -20,24 +20,3 @@ $location = "http://localhost:7071/api/JwtRedirect";
 
 echo '<a href="'.$location.'?token='.$encodeToken.'">link</a>';
 ?>
-
-
-<?php
-/*require 'jwt_helper.php';
-
-$secret_key = 'bmp_space_165423106546545';
-$token = array();
-$token['uid'] = $_GET['uid'];
-$token['firstname'] = $_GET['firstname'];
-$token['lastname'] = $_GET['lastname'];
-$token['exam_valid_for'] = $_GET['examvalidfor'];
-$token['exam_id'] = $_GET['examid'];
-$token['event_id'] = $_GET['eventid'];
-$token['exp'] = time() + $_GET['tokenvalidfor'] * 60; // how manu days is valid token
-
-$encodeToken = JWT::encode($token, $secret_key);
-$location = "http://localhost:7071/api/JwtRedirect";
-
-echo '<a href="'.$location.'?token='.$encodeToken.'">link</a>';*/
-
-?>
