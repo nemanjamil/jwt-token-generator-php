41bf4e7e38a84b1bc51b8599a9987d96f991a4f7
nemanjamil <nemanjamil@gmail.com>
2019-03-04 20:44:49 +0100

    Add redirect

--- generateToken.php
+++ generateToken.php
@@ -1,4 +1,7 @@
 <?php
+require 'vendor/autoload.php';
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 require 'jwt_helper.php';
 
 $secret_key = 'bmp_space_165423106546545';
@@ -6,18 +9,36 @@ $token = array();
 $token['Participant_EXTERNAL_ID'] = $_GET['Participant_EXTERNAL_ID'];
 $token['Participant_Firstname'] = $_GET['Participant_Firstname'];
 $token['Participant_Lastname'] = $_GET['Participant_Lastname'];
-
 $token['ExamVersion_EXTERNAL_ID'] = $_GET['ExamVersion_EXTERNAL_ID'];
 $token['BulkEvent_EXTERNAL_ID'] = $_GET['BulkEvent_EXTERNAL_ID'];
 $token['fe_endpoint'] = $_GET['fe_endpoint'];
-
 $token['ExamVersion_plannedDuration'] = $_GET['ExamVersion_plannedDuration'];
-
 $token['tokenvalidfor'] = time() + $_GET['tokenvalidfor'] * 86400; // e.g 7 days -> 7 * 86400  = 604800
 $token['ExamEvent_GenerationTime'] = time();
 
-$encodeToken = JWT::encode($token, $secret_key);
-$location = "http://localhost:7071/api/JwtRedirect";
+$ExamEvent_EXTERNAL_ID = $_GET['ExamEvent_EXTERNAL_ID'];
+if (!$ExamEvent_EXTERNAL_ID) {
+    try {
+        $token['ExamEvent_EXTERNAL_ID'] = Uuid::uuid1();
+    } catch (UnsatisfiedDependencyException $e) {
+        echo 'Caught exception: ' . $e->getMessage() . "\n";
+    }
+} else {
+    $token['ExamEvent_EXTERNAL_ID'] = $ExamEvent_EXTERNAL_ID;
+}
 
-echo '<a href="'.$location.'?token='.$encodeToken.'">link</a>';
+
+$encodeToken = JWT::encode($token, $secret_key);
+
+$location = "http://localhost:7071/api/JwtRedirect";
+$remoteLocation = "https://bpmuploadpictureandcognito.azurewebsites.net/api/JwtRedirect";
+
+echo '<a href="' . $remoteLocation . '?token=' . $encodeToken . '">link</a>';
+
+
+echo '</br></br>VALIDATE BODY</br></br>';
+
+$decoded = JWT::decode($encodeToken, $secret_key, array('HS256'));
+$decoded_array = (array) $decoded;
+var_dump($decoded_array);
 ?>
---
+++ redirect.php
@@ -0,0 +1,12 @@
+<?php
+echo "everything is ok";
+echo '</br>';
+echo "TOKEN";
+echo '</br>';
+echo $_GET["token"];
+
+echo '</br>';
+echo "status";
+echo '</br>';
+echo $_GET["status"];
+
